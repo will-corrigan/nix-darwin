fdab6d62ec16c1e25d34b4275ba67ca428ebcd4e
Will Corrigan <will-corrigan@users.noreply.github.com>
2026-03-25 23:50:05 +0000

    feat: conditional dotfile imports and SSH_AUTH_SOCK based on host config

--- modules/home.nix
+++ modules/home.nix
@@ -1,40 +1,42 @@
 {
   config,
   lib,
-  user,
+  host,
   ...
 }:
 let
-  username = config.system.primaryUser;
+  username = host.machine.username;
+  programs = host.programs or {};
+  integrations = host.integrations or {};
+
+  # Map program name + level to the correct dotfile path
+  programImport = name: level:
+    if level == "curated"
+    then ../dotfiles/curated/${name}.nix
+    else ../dotfiles/minimal/${name}.nix;
+
+  enabledImports = lib.mapAttrsToList programImport programs;
+
+  # Only set 1Password SSH agent if integration is configured
+  sessionVars =
+    if (integrations.ssh_signing or null) == "1password"
+    then {
+      SSH_AUTH_SOCK = "/Users/${username}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+    }
+    else {};
 in
 {
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
   home-manager.backupFileExtension = "backup";
-  home-manager.extraSpecialArgs = { inherit user; };
+  home-manager.extraSpecialArgs = { inherit host; };
 
   home-manager.users.${username} = {
     home.username = username;
     home.homeDirectory = lib.mkForce "/Users/${username}";
     home.stateVersion = "24.11";
+    home.sessionVariables = sessionVars;
 
-    home.sessionVariables = {
-      SSH_AUTH_SOCK = "/Users/${username}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
-    };
-
-    imports = [
-      ../dotfiles/zsh.nix
-      ../dotfiles/starship.nix
-      ../dotfiles/fzf.nix
-      ../dotfiles/bat.nix
-      ../dotfiles/btop.nix
-      ../dotfiles/ghostty.nix
-      ../dotfiles/zed.nix
-      ../dotfiles/tmux.nix
-      ../dotfiles/git.nix
-      ../dotfiles/direnv.nix
-      ../dotfiles/zoxide.nix
-      ../dotfiles/mise.nix
-    ];
+    imports = enabledImports;
   };
 }
